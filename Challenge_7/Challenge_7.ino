7bbaefe2fb091ad0250a36fc3dd0e3998e7cb439
André Vester Magnusson <77392011+Andr465m@users.noreply.github.com>
2023-11-01 09:45:27 +0100

    Make blank ino files

---
+++ Challenge_7/Challenge_7.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4Encoders encoders;
+Zumo32U4IMU imu;
+Zumo32U4LCD lcd;
+Zumo32U4Buzzer buzzer;
+Zumo32U4ButtonA buttonA;
+Zumo32U4Motors motors;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
