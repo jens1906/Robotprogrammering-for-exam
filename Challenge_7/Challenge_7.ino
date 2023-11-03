d7c77d43f0dc6721adb2571237518e5338aee5c7
Geekzter <jens1906@gmail.com>
2023-11-03 11:30:32 +0100

    Fungerer og er uden gyro, ren encoder, mangler kun at køre specifik afstand

--- Challenge_7/Challenge_7.ino
+++ Challenge_7/Challenge_7.ino
@@ -10,26 +10,13 @@ Zumo32U4IMU imu;
 Zumo32U4Motors motors;
 Zumo32U4OLED display;
 
-uint32_t turnAngle = 0;
-int16_t turnRate;
-int16_t gyroOffset;
-uint16_t gyroLastUpdate = 0;
-uint32_t CurrentAngle;
-int way;
-
-
 double EncoderArray[2];
 double ForwardVal[] = { 200, 200 };
 double MotorChange = 0.1;
-float EncoderMultipliers[] = { 1, 1.2 };
+float EncoderMultipliers[] = { 1, 0.996 };
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  turnSensorSetup();
-  delay(500);
-  turnSensorReset();
-  delay(2000);
 }
 
 void loop() {
@@ -44,13 +31,12 @@ void Encoders() {
   value = value + (countsLeft / countsRight);
 
   display.clear();
-  display.print((countsLeft + countsRight) / 2);
+  display.print(countsLeft);
   display.gotoXY(0, 1);
-  way = turnSensorUpdate();
-  display.print(way);
+  display.print(countsRight);
 
-  EncoderArray[0] = countsLeft;
-  EncoderArray[1] = (countsRight * 0.996);
+  EncoderArray[0] = (countsLeft * EncoderMultipliers[0]);
+  EncoderArray[1] = (countsRight * EncoderMultipliers[1]);
 }
 
 void CH7_ForwardToLine() {
@@ -70,38 +56,4 @@ void CH7_ForwardToLine() {
   ForwardVal[0], ForwardVal[1] = 200;
   encoders.getCountsAndResetLeft();
   encoders.getCountsAndResetRight();
-}
-
-void turnSensorReset() {
-  gyroLastUpdate = micros();
-  turnAngle = 0;
-}
-
-
-//Update gyro sensor kun med værdier fra 0-360
-uint32_t turnSensorUpdate() {
-  imu.readGyro();
-  turnRate = imu.g.z - gyroOffset;
-  uint16_t m = micros();
-  uint16_t dt = m - gyroLastUpdate;
-  gyroLastUpdate = m;
-  int32_t d = (int32_t)turnRate * dt;
-  turnAngle += (int64_t)d * 14680064 / 17578125;
-  return ((((uint32_t)turnAngle >> 16) * 360) >> 16);
-}
-
-void turnSensorSetup() {
-  Wire.begin();
-  imu.init();
-  imu.enableDefault();
-  imu.configureForTurnSensing();
-  delay(500);
-  int32_t total = 0;
-  for (uint16_t i = 0; i < 1024; i++) {
-    while (!imu.gyroDataReady()) {}
-    imu.readGyro();
-    total += imu.g.z;
-  }
-  gyroOffset = total / 1024;
-  turnSensorReset();
 }
