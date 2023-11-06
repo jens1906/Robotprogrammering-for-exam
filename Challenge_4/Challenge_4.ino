ebd254beea7a3efa15e554f52e44cf7b45b21ffa
Geekzter <jens1906@gmail.com>
2023-11-06 09:43:22 +0100

    Update Challenge_4.ino

--- Challenge_4/Challenge_4.ino
+++ Challenge_4/Challenge_4.ino
@@ -3,18 +3,63 @@
 
 Zumo32U4Encoders encoders;
 Zumo32U4IMU imu;
-Zumo32U4LCD lcd;
-Zumo32U4Buzzer buzzer;
-Zumo32U4ButtonA buttonA;
+Zumo32U4OLED display;
 Zumo32U4Motors motors;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+double value = 0;
+float wheelCirc = 11.938;
 
+double ForwardVal[] = { 200, 200 };
+double MotorChange = 0.1;
+float EncoderMultipliers[] = { 1, 0.996 };
+double EncoderArray[2];
+
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  CH4_Forward_Distance(10);
+  delay(1000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+double EncoderL() {
+  double countsLeft = abs(encoders.getCountsLeft());
+  countsLeft,EncoderArray[0] = (countsLeft * EncoderMultipliers[0]);
+  return countsLeft;
+}
+double EncoderR() {
+  double countsRight = abs(encoders.getCountsRight());
+  countsRight,EncoderArray[1] = (countsRight * EncoderMultipliers[1]);
+  return countsRight;
+}
+void Encoders(){
+  EncoderL();
+  EncoderR();
+}
 
+int CalcDistance(int Left, int Right) {
+  int Distance = (int)((((Left + Right) / 2) / (12 * 75)) * wheelCirc);
+  Serial.println(Distance);
+  return Distance;
+}
+
+void CH4_Forward_Distance(int Distance) {
+  Encoders();
+  while (Distance < CalcDistance(EncoderL, EncoderR)) {
+    motors.setSpeeds(ForwardVal[0], ForwardVal[1]);
+    Encoders();
+    if (EncoderArray[0] > EncoderArray[1]) {
+      ForwardVal[0] = ForwardVal[0] - MotorChange;
+      ForwardVal[1] = ForwardVal[1] + MotorChange;
+    } else if (EncoderArray[0] < EncoderArray[1]) {
+      ForwardVal[0] = ForwardVal[0] + MotorChange;
+      ForwardVal[1] = ForwardVal[1] - MotorChange;
+    }
+    delay(5);
+  }
+  motors.setSpeeds(0, 0);
+  ForwardVal[0], ForwardVal[1] = 200;
+  encoders.getCountsAndResetLeft();
+  encoders.getCountsAndResetRight();
 }
